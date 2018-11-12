d39c0e49abb052da6fdc5643ec601c8a6a3e3340
HayleyKnowlson <44553798+HayleyKnowlson@users.noreply.github.com>
2018-11-12 14:28:34 -0800

    Add files via upload
    Template v3.0

---
+++ form/contact.php
@@ -0,0 +1,68 @@
+<?php
+/* Set e-mail recipient */
+$myemail  = "sylvia.jamison647@gmail.com";
+
+/* Check all form inputs using check_input function */
+
+$yourname = check_input($_POST['yourname'], "Enter your name");
+$email  = check_input($_POST['email']);
+$emergency    = check_input($_POST['emergency']);
+$age  = check_input($_POST['age']);
+$gender   = check_input($_POST['gender']);
+$event = check_input($_POST['event']);
+$comments = check_input($_POST['comments'], "Write your comments");
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+    show_error("E-mail address not valid");
+}
+
+$message = "Hello!
+
+Your contact form has been submitted by:
+
+Name: $yourname
+E-mail: $email
+
+Comments:
+$comments
+
+End of message
+";
+/*echo $message;
+/* Send the message using mail() function */
+mail($myemail, "Registration", $message);
+
+/* Redirect visitor to the thank you page */
+header('Location: thanks.html');
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if ($problem && strlen($data) == 0)
+    {
+        show_error($problem);
+        echo "hello world";
+    }
+    return $data;
+}
+
+function show_error($myError)
+{
+?>
+    <html>
+    <body>
+
+    <b>Please correct the following error:</b><br />
+    <?php echo $myError; ?>
+
+    </body>
+    </html>
+<?php
+exit();
+}
+?>
